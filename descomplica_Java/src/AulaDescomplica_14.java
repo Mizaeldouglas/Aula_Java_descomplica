febc8fe88ffb7b7bf4a9d34c57e3ac99567238e1
Mizaeldouglas <mizaeldouglas2@gmail.com>
2022-04-23 20:50:17 -0300

    OlaMundo

--- descomplica_Java/src/AulaDescomplica_14.java
+++ descomplica_Java/src/AulaDescomplica_14.java
@@ -1,9 +1,25 @@
 
 public class AulaDescomplica_14 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String entrada[] ) {
 
+		int n1, n2;
+		int mod, div;
+		double raiz , pot;
+		String msg = "";
+
+		n1 = Integer.parseInt(entrada[0]);
+		n2 = Integer.parseInt(entrada[1]);
+		
+		
+		mod = n1 % n2;
+		div = (int)n1 / (int)n2;
+		raiz = Math.sqrt(n1);
+		pot = Math.pow(n1,n2);
+		
+		msg = "n1 ="+ n1 + " n2 = " + n2 + "\n";
+		
+		
 	}
 
 }
